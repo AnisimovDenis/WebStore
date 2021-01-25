b01b0a74cc867ee23523a396fd236b108e579a88
Денис Анисимов <den.anisimov.94@mail.ru>
2021-01-25 23:00:03 +0300

    Загатовка клиента для WebAPI управления сотрудниками

---
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Clients.Base;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        private readonly ILogger<EmployeesClient> logger;
+
+        public EmployeesClient(IConfiguration configuration, ILogger<EmployeesClient> logger) 
+            : base(configuration, "api/employees")
+        {
+            this.logger = logger;
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
