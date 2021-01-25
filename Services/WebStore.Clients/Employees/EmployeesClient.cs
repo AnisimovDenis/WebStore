a6947bb83cc3cdd877514dac85dea0ef8eab3db5
Денис Анисимов <den.anisimov.94@mail.ru>
2021-01-25 23:17:38 +0300

    Клиент для управления сотрудниками

--- Services/WebStore.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using WebStore.Clients.Base;
 using WebStore.Domain.Models;
@@ -19,29 +20,14 @@ namespace WebStore.Clients.Employees
             this.logger = logger;
         }
 
-        public IEnumerable<Employee> Get()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
 
-        public Employee Get(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
 
-        public int Add(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
+        public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
 
-        public void Update(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
+        public void Update(Employee employee) => Put(Address, employee);
 
-        public bool Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
     }
 }
