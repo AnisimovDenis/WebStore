dd13c147185931b859c95c2523c4c36f8fd45b34
Денис Анисимов <den.anisimov.94@mail.ru>
2021-01-25 22:55:28 +0300

    Логгирование данных

--- WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
+++ WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,14 +15,43 @@ namespace WebStore.ServiceHosting.Controllers
     public class EmployeesApiController : ControllerBase, IEmployeesData
     {
         private readonly IEmployeesData EmployeesData;
+        private readonly ILogger<EmployeesApiController> logger;
 
-        public EmployeesApiController(IEmployeesData EmployeesData) => this.EmployeesData = EmployeesData;
+        public EmployeesApiController(IEmployeesData EmployeesData, ILogger<EmployeesApiController> logger)
+        {
+            this.EmployeesData = EmployeesData;
+            this.logger = logger;
+        }
 
         [HttpPost]
-        public int Add(Employee employee) => EmployeesData.Add(employee);
+        public int Add(Employee employee)
+        {
+            logger.LogInformation($"Добавление сотрудника {employee.LastName} {employee.FirstName} {employee.Patronymic}");
+
+            var id = EmployeesData.Add(employee);
+
+            if (id > 0)
+                logger.LogInformation($"Cотрудник  [id:{employee.Id}] {employee.LastName} {employee.FirstName} {employee.Patronymic} успешно добавлен");
+            else
+                logger.LogError("Ошибка при добавлении");
+
+            return id;
+        }
 
         [HttpDelete("{id}")]
-        public bool Delete(int id) => EmployeesData.Delete(id);
+        public bool Delete(int id)
+        {
+            logger.LogInformation($"Удаление сотрудника [id:{id}]");
+
+            var deleteResult = EmployeesData.Delete(id);
+
+            if (deleteResult == true)
+                logger.LogInformation($"Сотрудник [id:{id}] успешно удален");
+            else
+                logger.LogInformation($"Ошибка при удалении");
+
+            return deleteResult;
+        }
 
         [HttpGet]
         public IEnumerable<Employee> Get() => EmployeesData.Get();
@@ -30,6 +60,16 @@ namespace WebStore.ServiceHosting.Controllers
         public Employee Get(int id) => EmployeesData.Get(id);
 
         [HttpPut]
-        public void Update(Employee employee) => EmployeesData.Update(employee);
+        public void Update(Employee employee)
+        {
+            logger.LogInformation($"Редактирование сотрудника {employee.LastName} {employee.FirstName} {employee.Patronymic}");
+
+            EmployeesData.Update(employee);
+
+            if (employee.Id > 0)
+                logger.LogInformation($"Cотрудник  [id:{employee.Id}] {employee.LastName} {employee.FirstName} {employee.Patronymic} успешно изменен");
+            else
+                logger.LogError("Ошибка при редактировании");
+        }
     }
 }
