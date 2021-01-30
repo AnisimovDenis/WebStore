72dd423510960639a1ee30bf61d4b0a65a64def1
Денис Анисимов <den.anisimov.94@mail.ru>
2021-01-30 14:12:37 +0300

    API управления ролями

--- WebStore.ServiceHosting/Controllers/Identity/UsersApiController.cs
+++ WebStore.ServiceHosting/Controllers/Identity/UsersApiController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
