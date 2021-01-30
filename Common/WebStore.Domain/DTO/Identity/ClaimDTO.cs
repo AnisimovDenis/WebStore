68db05ab9974a3bf3e52d1a203bd56156b66029c
Денис Анисимов <den.anisimov.94@mail.ru>
2021-01-30 14:10:23 +0300

    Остальное API управления пользователями

--- Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
+++ Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
@@ -12,9 +12,9 @@ namespace WebStore.Domain.DTO.Identity
         public IEnumerable<Claim> Claims { get; set; }
     }
 
-    public class AddClaim : ClaimDTO { }
+    public class AddClaimDTO : ClaimDTO { }
 
-    public class RemoveClaim : ClaimDTO { }
+    public class RemoveClaimDTO : ClaimDTO { }
 
     public class ReplaceClaimDTO : UserDTO 
     {
